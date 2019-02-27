627b1d0c189a0d35a78a259db4ccc18e2f223692
Pavel Dubrova <pashadubrova@gmail.com>
2019-02-27 04:40:54 +0200

    video: fbdev: msm: Implement display driver for In-Cell panels
    This driver provides support for specific Sony Xperia display panels.
    At this moment this driver can work with all Sony Xperia In-Cell panels.
    
    This driver is based on Yoshino's 47.2.A.6.30 kernel-copyleft display
    panel driver.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/video/fbdev/msm/mdss_panel.h
+++ drivers/video/fbdev/msm/mdss_panel.h
@@ -419,6 +419,10 @@ struct mipi_panel_info {
 	/* Dynamic Switch Support */
 	enum dynamic_mode_switch dms_mode;
 
+#ifdef CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL
+	bool switch_mode_pending;
+#endif
+
 	u32 pixel_packing;
 	u32 dsi_pclk_rate;
 	/* The packet-size should not bet changed */
@@ -782,6 +786,11 @@ struct mdss_panel_info {
 
 	/* HDR properties of display panel*/
 	struct mdss_panel_hdr_properties hdr_properties;
+
+#ifdef CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL
+	const char *panel_id_name;
+	int dsi_master;
+#endif
 };
 
 struct mdss_panel_timing {
@@ -816,6 +825,10 @@ struct mdss_panel_timing {
 
 	struct mdss_mdp_pp_tear_check te;
 	struct mdss_panel_roi_alignment roi_alignment;
+
+#ifdef CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL
+	bool koff_thshold_enable;
+#endif
 };
 
 struct mdss_panel_data {
@@ -823,6 +836,10 @@ struct mdss_panel_data {
 	void (*set_backlight)(struct mdss_panel_data *pdata, u32 bl_level);
 	int (*apply_display_setting)(struct mdss_panel_data *pdata, u32 mode);
 	unsigned char *mmss_cc_base;
+
+#ifdef CONFIG_FB_MSM_MDSS_SPECIFIC_PANEL
+	struct platform_device *panel_pdev;
+#endif
 
 	/**
 	 * event_handler() - callback handler for MDP core events
