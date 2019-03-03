81e115a4226435d6a36a4bad682d0deb364c4f2e
Pavel Dubrova <pashadubrova@gmail.com>
2019-03-03 22:27:27 +0200

    power: supply: qcom: Implement qpnp-fg extensions
    Was taken from 34.4.A.0.364 kernel-copyleft.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -344,7 +344,8 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(cc_soc),
 	POWER_SUPPLY_ATTR(qg_vbms_mode),
 	POWER_SUPPLY_ATTR(real_capacity),
-#ifdef CONFIG_QPNP_SMBCHARGER_EXTENSION
+#if defined(CONFIG_QPNP_SMBCHARGER_EXTENSION) || \
+    defined(CONFIG_QPNP_FG_EXTENSION)
 	POWER_SUPPLY_ATTR(usbin_det),
 	POWER_SUPPLY_ATTR(sub_type),
 	POWER_SUPPLY_ATTR(enable_shutdown_at_low_battery),
@@ -356,10 +357,13 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(lrc_not_startup),
 	POWER_SUPPLY_ATTR(max_charge_current),
 	POWER_SUPPLY_ATTR(int_cld),
+	POWER_SUPPLY_ATTR(batt_aging),
 	POWER_SUPPLY_ATTR(smart_charging_activation),
 	POWER_SUPPLY_ATTR(smart_charging_interruption),
 	POWER_SUPPLY_ATTR(smart_charging_status),
 	POWER_SUPPLY_ATTR(input_current_state),
+	POWER_SUPPLY_ATTR(charge_full_raw),
+	POWER_SUPPLY_ATTR(time_to_cap_learning),
 	POWER_SUPPLY_ATTR(chgerr_sts),
 #endif
 	/* Local extensions of type int64_t */
