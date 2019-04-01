7b4785b61659167bde5dfdc08c1a242a9a7de80c
Pavel Dubrova <pashadubrova@gmail.com>
2019-04-01 09:05:43 +0300

    misc: Inital NFC PN547 driver
    Inital NFC PN547 driver which is based on the driver from
    47.1.A.12.150 kernel-copyleft and was adapted to work
    with our PN547 chip.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

---
+++ include/linux/pn547.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2010 Trusted Logic S.A.
+ * Copyright (C) 2019 Pavel Dubrova <pashadubrova@gmail.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef _PN547_H_
+#define _PN547_H_
+
+#define PN547_DEVICE_NAME	"pn547"
+#define PN547_MAGIC		0xE9
+
+/*
+ * PN547 power control via ioctl
+ * PN547_SET_PWR(0): power off
+ * PN547_SET_PWR(1): power on
+ * PN547_SET_PWR(2): reset and power on with firmware download enabled
+ */
+#define PN547_SET_PWR		_IOW(PN547_MAGIC, 0x01, unsigned int)
+
+struct pn547_i2c_platform_data {
+	unsigned int irq_gpio;
+	unsigned int ven_gpio;
+	unsigned int firm_gpio;
+	unsigned int pvdd_en_gpio;
+	int gpio_fwdl_enable[4];
+	int gpio_fwdl_disable[4];
+};
+
+#endif
