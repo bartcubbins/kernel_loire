16750405da004e8cdc21063a0030f3c4746ce774
Pavel Dubrova <pashadubrova@gmail.com>
2019-11-08 03:05:49 +0200

    fbdev: msm: panel_driver: Remove redundant spec_pdata->ewu_seq check
    Address of 'spec_pdata->ewu_seq' will always evaluate to 'true'.
    Address of 'dev->kobj' will always evaluate to 'true'.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/video/fbdev/msm/panel_driver/panel_driver.c
+++ drivers/video/fbdev/msm/panel_driver/panel_driver.c
@@ -259,10 +259,7 @@ static int panel_calculation_sleep(
 	if (panel_driver_is_seq_for_ewu() &&
 		(gpio == spec_pdata->touch_reset_gpio) &&
 		!enable) {
-		if (&spec_pdata->ewu_seq)
 			pw_seq = &spec_pdata->ewu_seq;
-		else
-			pw_seq = &spec_pdata->on_seq;
 	} else {
 		if (enable)
 			pw_seq = &spec_pdata->on_seq;
@@ -959,9 +956,7 @@ int panel_driver_reset_panel(struct mdss_panel_data *pdata, int enable)
 	}
 
 	if (panel_driver_is_seq_for_ewu() && enable)
-		pw_seq = &ctrl_pdata->spec_pdata->ewu_seq ?
-				&ctrl_pdata->spec_pdata->ewu_seq :
-				&ctrl_pdata->spec_pdata->on_seq;
+		pw_seq = &ctrl_pdata->spec_pdata->ewu_seq;
 	else
 		pw_seq = (enable) ? &ctrl_pdata->spec_pdata->on_seq :
 					&ctrl_pdata->spec_pdata->off_seq;
