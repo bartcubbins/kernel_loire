bfb528dc2503df5076892c8e2b1ca78cc845d576
Pavel Dubrova <pashadubrova@gmail.com>
2019-11-28 10:52:10 +0200

    Revert "drivers: soc: qcom: Peripheral image for legacy devices"
    This reverts commit c0d4360d6718af3af523e042d9f60163bc591bbc.
    
    Issue was fixed in previous commit and this
    workaround is no longer needed.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/soc/qcom/peripheral-loader.c
+++ drivers/soc/qcom/peripheral-loader.c
@@ -340,8 +340,7 @@ EXPORT_SYMBOL(pil_do_ramdump);
 int pil_assign_mem_to_subsys(struct pil_desc *desc, phys_addr_t addr,
 							size_t size)
 {
-	int ret = 0;
-#ifndef CONFIG_MSM_PIL_LEGACY
+	int ret;
 	int srcVM[1] = {VMID_HLOS};
 	int destVM[1] = {desc->subsys_vmid};
 	int destVMperm[1] = {PERM_READ | PERM_WRITE};
@@ -350,7 +349,6 @@ int pil_assign_mem_to_subsys(struct pil_desc *desc, phys_addr_t addr,
 	if (ret)
 		pil_err(desc, "%s: failed for %pa address of size %zx - subsys VMid %d rc:%d\n",
 				__func__, &addr, size, desc->subsys_vmid, ret);
-#endif
 	return ret;
 }
 EXPORT_SYMBOL(pil_assign_mem_to_subsys);
@@ -358,8 +356,7 @@ EXPORT_SYMBOL(pil_assign_mem_to_subsys);
 int pil_assign_mem_to_linux(struct pil_desc *desc, phys_addr_t addr,
 							size_t size)
 {
-	int ret = 0;
-#ifndef CONFIG_MSM_PIL_LEGACY
+	int ret;
 	int srcVM[1] = {desc->subsys_vmid};
 	int destVM[1] = {VMID_HLOS};
 	int destVMperm[1] = {PERM_READ | PERM_WRITE | PERM_EXEC};
@@ -368,7 +365,7 @@ int pil_assign_mem_to_linux(struct pil_desc *desc, phys_addr_t addr,
 	if (ret)
 		panic("%s: failed for %pa address of size %zx - subsys VMid %d rc:%d\n",
 				__func__, &addr, size, desc->subsys_vmid, ret);
-#endif
+
 	return ret;
 }
 EXPORT_SYMBOL(pil_assign_mem_to_linux);
@@ -376,8 +373,7 @@ EXPORT_SYMBOL(pil_assign_mem_to_linux);
 int pil_assign_mem_to_subsys_and_linux(struct pil_desc *desc,
 						phys_addr_t addr, size_t size)
 {
-	int ret = 0;
-#ifndef CONFIG_MSM_PIL_LEGACY
+	int ret;
 	int srcVM[1] = {VMID_HLOS};
 	int destVM[2] = {VMID_HLOS, desc->subsys_vmid};
 	int destVMperm[2] = {PERM_READ | PERM_WRITE, PERM_READ | PERM_WRITE};
@@ -386,7 +382,6 @@ int pil_assign_mem_to_subsys_and_linux(struct pil_desc *desc,
 	if (ret)
 		pil_err(desc, "%s: failed for %pa address of size %zx - subsys VMid %d rc:%d\n",
 				__func__, &addr, size, desc->subsys_vmid, ret);
-#endif
 
 	return ret;
 }
@@ -395,8 +390,7 @@ EXPORT_SYMBOL(pil_assign_mem_to_subsys_and_linux);
 int pil_reclaim_mem(struct pil_desc *desc, phys_addr_t addr, size_t size,
 						int VMid)
 {
-	int ret = 0;
-#ifndef CONFIG_MSM_PIL_LEGACY
+	int ret;
 	int srcVM[2] = {VMID_HLOS, desc->subsys_vmid};
 	int destVM[1] = {VMid};
 	int destVMperm[1] = {PERM_READ | PERM_WRITE};
@@ -408,7 +402,6 @@ int pil_reclaim_mem(struct pil_desc *desc, phys_addr_t addr, size_t size,
 	if (ret)
 		panic("%s: failed for %pa address of size %zx - subsys VMid %d. Fatal error.\n",
 				__func__, &addr, size, desc->subsys_vmid);
-#endif
 
 	return ret;
 }
