4362c803771c6e4e31af186c231fc9ae80707f30
Pavel Dubrova <pashadubrova@gmail.com>
2020-01-19 04:03:52 +0200

    iommu: qcom: Cleanup headers
    Delete unused registers. In fact, we could use the common
    msm_iommu_hw-8xxx.h header, but in the future we will
    migrate to the new IOMMU driver, so I decided to just
    clean the current headers.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- include/linux/qcom_iommu.h
+++ include/linux/qcom_iommu.h
@@ -19,19 +19,6 @@
 #define DOMAIN_ATTR_QCOM_COHERENT_HTW_DISABLE	DOMAIN_ATTR_MAX
 
 #ifdef CONFIG_QCOM_IOMMU_V1
-void *msm_iommu_lock_initialize(void);
-void msm_iommu_mutex_lock(void);
-void msm_iommu_mutex_unlock(void);
-#else
-static inline void *msm_iommu_lock_initialize(void)
-{
-	return NULL;
-}
-static inline void msm_iommu_mutex_lock(void) { }
-static inline void msm_iommu_mutex_unlock(void) { }
-#endif
-
-#ifdef CONFIG_QCOM_IOMMU_V1
 /*
  * Look up an IOMMU context device by its context name. NULL if none found.
  * Useful for testing and drivers that do not yet fully have IOMMU stuff in
@@ -45,54 +32,11 @@ static inline struct device *msm_iommu_get_ctx(const char *ctx_name)
 }
 #endif
 
-#ifdef CONFIG_MSM_IOMMU_V0
-static inline int msm_soc_version_supports_iommu_v0(void)
-{
-	static int soc_supports_v0 = -1;
-#ifdef CONFIG_OF
-	struct device_node *node;
-#endif
-
-	if (soc_supports_v0 != -1)
-		return soc_supports_v0;
-
-#ifdef CONFIG_OF
-	node = of_find_compatible_node(NULL, NULL, "qcom,msm-smmu-v0");
-	if (node) {
-		soc_supports_v0 = 1;
-		of_node_put(node);
-		return 1;
-	}
-#endif
-	if (cpu_is_msm8960() &&
-	    SOCINFO_VERSION_MAJOR(socinfo_get_version()) < 2) {
-		soc_supports_v0 = 0;
-		return 0;
-	}
-
-	if (cpu_is_msm8x60() &&
-	    (SOCINFO_VERSION_MAJOR(socinfo_get_version()) != 2 ||
-	    SOCINFO_VERSION_MINOR(socinfo_get_version()) < 1))	{
-		soc_supports_v0 = 0;
-		return 0;
-	}
-
-	soc_supports_v0 = 1;
-	return 1;
-}
-#else
-static inline int msm_soc_version_supports_iommu_v0(void)
-{
-	return 0;
-}
-#endif
-
 extern struct bus_type platform_bus_type;
 static inline struct bus_type *msm_iommu_get_bus(struct device *dev)
 {
 
 	return &platform_bus_type;
 }
-
 
 #endif
